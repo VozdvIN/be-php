cd58fe75efccacb66280f11b81147f255e71abd6
VozdvIN <vozdvin@yandex.ru>
2014-04-17 14:16:02 +0400

    add: Постоянная ссылка на сайт проекта в подвале сайта.

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,3 +1,8 @@
 <div>
-  <?php include ('customization/footer.php') ?>
+  <div>
+    <?php include ('customization/footer.php') ?>
+  </div>
+  <div style="font-size: smaller; text-align:right;">
+  	Построено на основе <a href="http://beavengine.ru" target="_blank">Beaver's Engine</a>.
+  </div>
 </div>
