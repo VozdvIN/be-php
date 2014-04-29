32e935a24ad046cb2dbeb037a24f413cea59251b
VozdvIN <vozdvin@yandex.ru>
2014-04-30 00:55:11 +0400

    sys: версия 0.16.3b

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -2,7 +2,7 @@
   <div>
     <?php include ('customization/footer.php') ?>
   </div>
-  <div style="font-size: smaller; text-align:right;">
-  	Построено на основе <a href="http://beavengine.ru" target="_blank">Beaver's Engine</a>.
+  <div style="font-size: smaller; text-align: right; margin-top: 0.5em">
+  	 Powered by <a href="http://beavengine.ru" target="_blank">Beaver's Engine</a> 0.16.3b.
   </div>
 </div>
