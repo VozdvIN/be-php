ede828a85f7189806bf982bcd28d8cf3adb5bfef
VozdvIN <vozdvin@yandex.ru>
2014-12-07 22:00:54 +0300

    fix: UI модуля team

--- apps/frontend/modules/team/templates/editSuccess.php
+++ apps/frontend/modules/team/templates/editSuccess.php
@@ -1,3 +1,3 @@
 <h2>Правка свойств команды &quot;<?php echo $form->getObject()->name; ?>&quot;</h2>
 
-<?php include_partial('global/formCrud', array('form' => $form, 'mudule' => 'team')) ?>
+<?php include_partial('global/formCrud', array('form' => $form, 'module' => 'team')) ?>
