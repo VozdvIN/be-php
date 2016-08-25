631cc86d2396d3c95028132245d3b37f736024e0
VozdvIN <vozdvin@yandex.ru>
2016-08-25 16:44:02 +0300

    chg: верстка главной страницы

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,21 +1,15 @@
 <div class="border-top">
-	<div style="display: inline-block; width: 50%">
-		<div style="text-align: left;">
-			<p>
-				Время сервера: <span id="serverTime">--:--:--</span>.
-			</p>
-		</div>
+	<div style="display: inline-block; width: 30%"><!--
+	 --><p>
+			Время сервера: <span id="serverTime">--:--:--</span>.
+		</p>
+		<p class="pad-top">
+			Powered by <a href="http://beavengine.ru" target="_blank">Beaver's Engine</a>.
+		</p>
 	</div><!--
-	 --><div style="display: inline-block; width: 50%">
-		<div style="text-align: right;">
-			<div style="display: inline-block;">
-				<article>
-					<?php echo ($footerArticle = Article::byName('Шаблонные-Подвал')) ? Utils::decodeBB($footerArticle->text) : '(Заполните статью \'Шаблонные-Подвал\')'; ?>  
-				</article>
-				<p class="border-top">
-					Powered by <a href="http://beavengine.ru" target="_blank">Beaver's Engine</a>.
-				</p>
-			</div>
-		</div>
+ --><div style="display: inline-block; width: 70%">
+		<article>
+			<?php echo ($footerArticle = Article::byName('Шаблонные-Подвал')) ? Utils::decodeBB($footerArticle->text) : '(Заполните статью \'Шаблонные-Подвал\')'; ?>  
+		</article>
 	</div>
 </div>
--- apps/frontend/templates/_header.php
+++ apps/frontend/templates/_header.php
@@ -10,26 +10,26 @@ $showModeration = $sessionWebUser && $sessionWebUser->hasSomeToModerate();
 		</a>
 	</div><!--
  --><div style="display: inline-block; padding-left: 6px;">
-		<h1 style="text-align: left; padding: 0 0 0 0;"><?php echo SiteSettings::SITE_NAME ?></h1>
+		<h1 style="text-align: left; padding: 0 0 0 0;"><?php echo SiteSettings::SITE_NAME.' - '.Region::byId($sf_user->getAttribute('region_id'))->name ?></h1>
 	</div><!--
  --><nav class="menu">
 		<ul>
 			<?php if ($sf_user->isAuthenticated()): ?>
 			<li><?php echo link_to('Главная', '/home/index') ?></li><!--
+		 --><li><?php echo link_to('Профиль', 'webUser/show?id='.$sf_user->getAttribute('id')) ?></li><!--
+		 --><li><?php echo link_to('Проект', 'region/setCurrent?returl='.Utils::encodeSafeUrl($_SERVER['REQUEST_URI'])) ?></li><!--
 		 --><li><?php echo link_to('Команды', 'team/index') ?></li><!--
 		 --><li><?php echo link_to('Игры', 'game/index') ?></li><!--
 		 --><li><?php echo link_to('Участники', 'webUser/index') ?></li><!--
-		 --><li><?php echo link_to('Статьи', 'article/by?name=Разделы') ?></li><!--
-		 --><li><?php echo link_to('Проект:'.Region::byId($sf_user->getAttribute('region_id'))->name, 'region/setCurrent?returl='.Utils::encodeSafeUrl($_SERVER['REQUEST_URI'])) ?></li><!--
-		 --><li><?php echo link_to('Профиль:'.$sf_user->getAttribute('login'), 'webUser/show?id='.$sf_user->getAttribute('id')) ?></li><!--
 			<?php	if ($showModeration): ?>
 		 --><li><?php	echo link_to('Модерирование', 'moderation/show') ?></li><!--
-			<?php	endif; ?><!--		 
-		 --><li><?php echo link_to('Выход', 'auth/logout') ?></li>
+			<?php	endif; ?>
+		 --><li><?php echo link_to('Статьи', 'article/by?name=Разделы') ?></li><!--
+		 --><li><?php echo link_to('Выход', 'auth/logout') ?></li><!--
 			<?php else: ?>
-			<li><?php echo link_to('Вход', 'auth/login') ?></li><!--
-		 --><li><?php echo link_to('Регистрация', 'auth/register') ?></li>
+		 --><li><?php echo link_to('Вход', 'auth/login') ?></li><!--
+		 --><li><?php echo link_to('Регистрация', 'auth/register') ?></li><!--
 			<?php endif; ?>
-		</ul>
+	 --></ul>
 	</nav>
 </div>
