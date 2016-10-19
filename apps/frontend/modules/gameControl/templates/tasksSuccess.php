3e83a0cd7513c4c6eb93eaaad524427ce29edb55
VozdvIN <vozdvin@yandex.ru>
2016-10-19 23:34:00 +0400

    chg: управление игрой - управление текущими заданиями

--- apps/frontend/modules/gameControl/templates/tasksSuccess.php
+++ apps/frontend/modules/gameControl/templates/tasksSuccess.php
@@ -14,24 +14,39 @@
 			<td>
 				<?php echo link_to($teamState->Team->name, 'team/show?id='.$teamState->Team->id, array('target' => '_blank')); ?>
 			</td>
-			<?php if ($currentTaskState): ?>
 			<td>
+				<?php if ($currentTaskState): ?>
 				<?php echo link_to($task->name, 'task/params?id='.$task->id, array('target' => '_blank')); ?>
+				<?php else: ?>
+					<?php if ($teamState->status == TeamState::TEAM_FINISHED): ?>
+				<span class="info" style="font-style: italic;"><?php echo link_to('Финишировала', 'play/task?id='.$teamState->id, array('target' => '_blank')) ?></span>
+					<?php else: ?>
+				<span class="warn" style="font-style: italic;"><?php echo link_to('Нет&nbsp;задания', 'play/task?id='.$teamState->id, array('target' => '_blank')) ?></span>
+					<?php endif; ?>
+				<?php endif; ?>
 			</td>
 			<td>
+				<?php if ($currentTaskState): ?>
 				<span><?php echo link_to($currentTaskState->describeStatus(), 'play/task?id='.$teamState->id, array('target' => '_blank')); ?></span>
-				<?php if ($currentTaskState->Task->manual_start): ?>
+					<?php if ($currentTaskState->Task->manual_start): ?>
 				<span class="warn">,&nbsp;вручную</span>
-				<?php else: ?>
+					<?php else: ?>
 				<span class="info">,&nbsp;автостарт</span>
-				<?php endif; ?>
-				<?php if (($currentTaskState->status == TaskState::TASK_GIVEN) && $currentTaskState->Task->isFilled()): ?>
+					<?php endif; ?>
+					<?php if ($currentTaskState->Task->isFilled()): ?>
 				<span class="warn">,&nbsp;заполнено</span>
-				<?php endif ?>
+					<?php endif ?>
+				<?php else: ?>
+					<?php if ($teamState->ai_enabled): ?>
+				<span class="info">Автовыбор</span>
+					<?php elseif ($teamState->status != TeamState::TEAM_FINISHED): ?>
+				<span class="warn">ИИ&nbsp;выключен</span>
+					<?php endif ?>
+				<?php endif; ?>
 			</td>
 			<td>
 				<?php
-					if ($_isManager)
+					if ($currentTaskState && $_isManager)
 					{
 						include_partial(
 							'global/actionsMenu',
@@ -40,7 +55,7 @@
 									'start' => link_to('Старт', 'taskState/start?id='.$currentTaskState->id, array('method' => 'post', 'confirm' => 'Дать старт заданию '.$task->name.' для команды '.$teamState->Team->name.' ?')),
 									'cancel' => link_to('Отменить','teamState/abandonTask?id='.$teamState->id, array('method' => 'post', 'confirm' => 'Отменить выдачу задания '.$task->name.' командe '.$teamState->Team->name.' ?')),
 									'accept' => link_to('Прочесть','taskState/forceAccept?id='.$currentTaskState->id, array('method' => 'post', 'confirm' => 'Подтвердить прочтение задания '.$task->name.' командой '.$teamState->Team->name.' ?')),
-									'abort' => link_to('Прекратить','teamState/abandonTask?id='.$teamState->id, array('method' => 'post', 'confirm' => 'Прекратить выполнение задания '.$task->name.' командой '.$teamState->Team->name.' ?')),
+									'abort' => link_to('Прекратить','teamState/abandonTask?id='.$teamState->id, array('method' => 'post', 'confirm' => 'Прекратить выполнение задания '.$task->name.' командой '.$teamState->Team->name.' ?'))
 								),
 								'css' => array(
 									'start' => 'warn',
@@ -49,11 +64,7 @@
 									'abort' => 'danger'
 								),
 								'conditions' => array(
-									'start' => $currentTaskState->status == TaskState::TASK_GIVEN
-												&& (
-													($currentTaskState->Task->isFilled()
-													|| $currentTaskState->Task->manual_start)
-												),
+									'start' => $currentTaskState->status == TaskState::TASK_GIVEN,
 									'cancel' => $currentTaskState->status == TaskState::TASK_GIVEN
 												|| $currentTaskState->status == TaskState::TASK_STARTED,
 									'accept' => $currentTaskState->status == TaskState::TASK_STARTED,
@@ -63,19 +74,10 @@
 						);
 					}
 				?>
-			</td>
-			<?php else: ?>
-			<td colspan="3">
-				<?php if ($teamState->status == TeamState::TEAM_FINISHED): ?>
-				<span class="info"><?php echo link_to('Финишировала', 'play/task?id='.$teamState->id, array('target' => '_blank')) ?></span>
-				<?php else: ?>
-				<span class="warn"><?php echo link_to('Нет&nbsp;задания', 'play/task?id='.$teamState->id, array('target' => '_blank')) ?></span>
-				<?php     if ($_isManager && ($teamState->status == TeamState::TEAM_WAIT_TASK)): ?>
+				<?php if ( ! $currentTaskState && $teamState->status == TeamState::TEAM_WAIT_TASK): ?>
 				<span class="button-danger"><?php echo link_to('Финишировать','teamState/forceFinish?id='.$teamState->id, array('method' => 'post', 'confirm' => 'Отправить команду '.$teamState->Team->name.' на финиш?')) ?></span>
-				<?php     endif; ?>
 				<?php endif; ?>
 			</td>
-			<?php endif; ?>
 		</tr>
 		<?php endforeach ?>
 	</tbody>
